c78286833dcebcc28bd7c26b4ccc88120e0811a5
Halex Viotto Gomes <halexx002@gmail.com>
2021-06-15 14:51:59 -0400

    Challenge done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,66 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const [repository] = repositories.filter(
+    (repository) => repository.id === id
+  );
+
+  if (!repository) return response.status(400).end();
+
+  if (title != null) repository.title = title;
+  if (url != null) repository.url = url;
+  if (techs != null) repository.techs = techs.slice();
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+
+  if (index === -1) return response.status(400).end();
+
+  repositories.splice(index, 1);
+
+  return response.status(204).end();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const [repository] = repositories.filter(
+    (repository) => repository.id === id
+  );
+
+  if (!repository) return response.status(400).end();
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
